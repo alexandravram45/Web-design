8d48c81d33a98908430b251eaede6bf7a4adbb41
alexandravram45 <74004954+alexandravram45@users.noreply.github.com>
2021-02-23 19:26:04 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+
 var data = [
     { productName: "Leaf Rake", code: "gdn 0011", availableDate: "March 19", price: "$19,95", starRating: "4" },
     { productName: "Garden Cart", code: "gdn 0023", availableDate: "March 18", price: "$32,99", starRating: "5" },
     { productName: "Hammer", code: "tbx 0048", availableDate: "March 19", price: "$8,90", starRating: "3" },
     { productName: "Saw", code: "tbx 0022", availableDate: "March 19", price: "$11,55", starRating: "3" }
 ];
-window.onload = function () {
+var menu = document.querySelector("nav > a");
+var table = document.querySelector("table");
+var a1 = document.getElementById("a1");
+var a2 = document.getElementById("a2");
+menu.addEventListener('click', function () {
+    a1.style.visibility = "visible";
+    a2.style.visibility = "visible";
+});
+function createTable() {
     var table = document.querySelector("tbody");
     data.forEach(function (prod) {
         var row = document.createElement("tr");
@@ -17,17 +26,36 @@ window.onload = function () {
         table.appendChild(row);
         var btnDelete = document.createElement("button");
         var btnDetails = document.createElement("button");
-        btnDetails.innerHTML = "<i class=\"fas fa-pen\"></i><a href=\"#\">" + "Details";
-        btnDelete.innerHTML = "<i class=\"fas fa-trash-alt\"></i><a href=\"#\">" + "Delete";
+        btnDetails.innerHTML = "<i class=\"fas fa-pen\"></i><a href=\"#\">" + " Details";
+        btnDelete.innerHTML = "<i class=\"fas fa-trash-alt\"></i><a href=\"#\">" + " Delete";
         row.insertCell();
         var lastCell = document.querySelector("tr:last-child");
         lastCell.appendChild(btnDelete);
         lastCell.appendChild(btnDetails);
         btnDelete.addEventListener('click', function () {
-            var index = row.rowIndex;
-            data.splice(index - 1, 1);
-            console.log(data);
-            table.rows[index].remove();
+            data.splice(row.rowIndex - 1, 1);
+            table.rows[row.rowIndex].remove();
         });
     });
+}
+;
+window.onload = createTable;
+/*  Http request
+
+function req () {
+ const request = new XMLHttpRequest();
+ request.open('GET', 'localhost:3333/products');
+
+ request.onload = function() {
+   if(request.status === 200) {
+       let text = JSON.parse(request.responseText);
+         createTable;
+       }
+    else {
+       console.log("Error loading groups: " + request.status + " - "+ request.statusText);
+   }
 };
+ request.send(null);
+}
+req;
+*/
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-
+export {};
 const data = [
     { productName: "Leaf Rake", code: "gdn 0011", availableDate: "March 19", price: "$19,95", starRating: "4"},
     { productName: "Garden Cart", code: "gdn 0023", availableDate: "March 18", price: "$32,99", starRating: "5" },
@@ -7,7 +7,20 @@ const data = [
    
   ];
 
- window.onload = function () {
+
+let menu = document.querySelector("nav > a");
+let table = document.querySelector("table");
+let a1 = document.getElementById("a1");
+let a2 = document.getElementById("a2");
+menu.addEventListener('click', function(){
+    a1.style.visibility = "visible";
+    a2.style.visibility = "visible";
+});
+
+
+
+
+function createTable () {
     const table = document.querySelector("tbody");
     data.forEach(prod => {
       const row = document.createElement("tr");
@@ -22,25 +35,44 @@ const data = [
       
       let btnDelete = document.createElement("button") as HTMLButtonElement; 
       let btnDetails = document.createElement("button") as HTMLButtonElement;
-      btnDetails.innerHTML = "<i class=\"fas fa-pen\"></i><a href=\"#\">" + "Details";
-      btnDelete.innerHTML = "<i class=\"fas fa-trash-alt\"></i><a href=\"#\">" + "Delete";
+      btnDetails.innerHTML = "<i class=\"fas fa-pen\"></i><a href=\"#\">" + " Details";
+      btnDelete.innerHTML = "<i class=\"fas fa-trash-alt\"></i><a href=\"#\">" + " Delete";
       row.insertCell();
       let lastCell = document.querySelector("tr:last-child");
       lastCell.appendChild(btnDelete);
       lastCell.appendChild(btnDetails);
       
       btnDelete.addEventListener('click', function() {
-        let index = row.rowIndex;
-        data.splice(index - 1, 1);
-        console.log(data);
-        table.rows[index].remove();
+        data.splice(row.rowIndex - 1, 1);
+        table.rows[row.rowIndex].remove();
        
       });
    });
       
- };
- 
+};
 
+window.onload = createTable;
+
+
+ /*  Http request
+ 
+ function req () {
+  const request = new XMLHttpRequest();
+  request.open('GET', 'localhost:3333/products');
+ 
+  request.onload = function() {
+    if(request.status === 200) {
+        let text = JSON.parse(request.responseText);
+          createTable;
+        }
+     else {
+        console.log("Error loading groups: " + request.status + " - "+ request.statusText);
+    }
+};
+  request.send(null);
+}
+req;
+*/
 
  
  
